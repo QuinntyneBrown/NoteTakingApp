6cb30fb9c15bb7d4afddbea9905a4f93013458b8
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2018-06-23 18:03:15 -0400

    update

--- src/NoteTakingApp.Core/IntegrationEventsHub.cs
+++ src/NoteTakingApp.Core/IntegrationEventsHub.cs
@@ -9,18 +9,24 @@ using System.Threading.Tasks;
 
 namespace NoteTakingApp.Core
 {
+    public static class ConnectedUsers {
+        public static ConcurrentDictionary<string, byte> Value = new ConcurrentDictionary<string, byte>();
+    }
+
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class IntegrationEventsHub: Hub
     {
         private IAccessTokenRepository _repository;
-        private static ConcurrentDictionary<string,byte> _connectedUsers = new ConcurrentDictionary<string, byte>();
+        
+        public IntegrationEventsHub(IAccessTokenRepository repository) {
+            _repository = repository;
+        }
 
-        public IntegrationEventsHub(IAccessTokenRepository repository) => _repository = repository;
         public string UserName => Context.User.Identity.Name;
 
         public override async Task OnConnectedAsync()
         {            
-            if (!_connectedUsers.TryAdd(UserName,0))
+            if (!ConnectedUsers.Value.TryAdd(UserName,0))
             {
                 await _repository.InvalidateByUsernameAsync(UserName);
                 await _repository.SaveChangesAsync(default(CancellationToken));
@@ -32,8 +38,8 @@ namespace NoteTakingApp.Core
 
         public override Task OnDisconnectedAsync(Exception exception)
         {
-            _connectedUsers.TryRemove(UserName, out _);
+            ConnectedUsers.Value.TryRemove(UserName, out _);
             return base.OnDisconnectedAsync(exception);
         }        
-    }
+    }    
 }
